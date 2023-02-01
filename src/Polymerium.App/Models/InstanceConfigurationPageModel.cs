7f057570e370fc87db754422db31026134ba8e96
Chien Zhang <zqy0224@live.com>
2023-02-02 01:55:58 +0800

    资源文件怎么也没提交上去

---
+++ src/Polymerium.App/Models/InstanceConfigurationPageModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymerium.App.Models
+{
+    public class InstanceConfigurationPageModel
+    {
+        public string IconSource { get; set; }
+        public string Header { get; set; }
+        public Type Page { get; set; }
+    }
+}
---
+++ src/Polymerium.App/ViewModels/Instances/InstanceConfigurationViewModel.cs
@@ -0,0 +1,41 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Polymerium.Abstractions;
+using Polymerium.App.Models;
+using Polymerium.App.Services;
+using System.Collections.ObjectModel;
+using Polymerium.App.Views.Instances;
+
+namespace Polymerium.App.ViewModels.Instances;
+
+public class InstanceConfigurationViewModel : ObservableObject
+{
+    private readonly ViewModelContext _context;
+    public ViewModelContext Context => _context;
+    public ObservableCollection<InstanceConfigurationPageModel> Pages { get; set; }
+
+    public InstanceConfigurationViewModel(ViewModelContext context)
+    {
+        _context = context;
+        Pages = new()
+        {
+            new()
+            {
+                Header = "元数据",
+                IconSource = "ms-appx:///Assets/Icons/icons8-blueprint-48.png",
+                Page = typeof(InstanceMetadataConfigurationView)
+            },
+            new()
+            {
+                Header = "启动参数",
+                IconSource = "ms-appx:///Assets/Icons/icons8-firework-48.png",
+                Page = typeof(InstanceLaunchConfigurationView)
+            },
+            new()
+            {
+                Header = "高级",
+                IconSource = "ms-appx:///Assets/Icons/icons8-slider-48.png",
+                Page = typeof(InstanceAdvancedConfigurationView)
+            }
+        };
+    }
+}
