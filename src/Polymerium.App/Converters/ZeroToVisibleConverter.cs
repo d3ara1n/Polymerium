5fb9eba75fecfeb594acce04a5013fcb7da45fa5
Chien Zhang <zqy0224@live.com>
2023-04-15 18:18:16 +0800

    空列表提示能正确显示了

---
+++ src/Polymerium.App/Converters/ZeroToVisibleConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+
+namespace Polymerium.App.Converters;
+
+public class ZeroToVisibleConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        return value switch
+        {
+            int it => it == 0,
+            long it => it == 0,
+            double it => Math.Abs(it) < 0.001,
+            float it => Math.Abs(it) < 0.001,
+            uint it => it == 0,
+            ulong it => it == 0,
+            _ => false
+        }
+            ? Visibility.Visible
+            : Visibility.Collapsed;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
+    }
+}
