f6f0e484ce149439e6bea9b03db1428819ac9b88
Chien Zhang <zqy0224@live.com>
2023-04-15 20:40:02 +0800

    加了个意义不明的新闻页

---
+++ src/Polymerium.App/Models/HomeNewsItemModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Polymerium.App.Models;
+
+public class HomeNewsItemModel
+{
+    public HomeNewsItemModel(string title, string text, DateTimeOffset date, Uri? readMore, string imageSource)
+    {
+        Title = title;
+        Text = text;
+        Date = date;
+        ReadMore = readMore;
+        ImageSource = imageSource;
+    }
+
+    public string Title { get; set; }
+    public string Text { get; set; }
+    public DateTimeOffset Date { get; set; }
+    public Uri? ReadMore { get; set; }
+    public string ImageSource { get; set; }
+}
