b4d3fe8a83bfc2da48409137e600504c081c54c4
Chien Zhang <zqy0224@live.com>
2023-04-30 15:37:47 +0800

    fmt

--- src/Polymerium.App/Views/InstanceUpdateDialog.xaml.cs
+++ src/Polymerium.App/Views/InstanceUpdateDialog.xaml.cs
@@ -1,24 +1,10 @@
 // Copyright (c) Microsoft Corporation and Contributors.
 // Licensed under the MIT License.
 
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Polymerium.App.Controls;
-using Polymerium.App.ViewModels;
 using Microsoft.Extensions.DependencyInjection;
+using Polymerium.App.Controls;
 using Polymerium.App.Models;
+using Polymerium.App.ViewModels;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -38,5 +24,10 @@ namespace Polymerium.App.Views
         }
 
         public InstanceUpdateViewModel ViewModel { get; }
+
+        private void CancelButton_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+        {
+            Dismiss();
+        }
     }
 }
