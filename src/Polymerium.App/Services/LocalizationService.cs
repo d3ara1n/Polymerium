b788f1e49e2f65bde89621dd463ac8cd3b48b7b4
Chien Zhang <zqy0224@live.com>
2023-04-30 21:21:25 +0800

    又英化了一点

---
+++ src/Polymerium.App/Services/LocalizationService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Windows.ApplicationModel.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymerium.App.Services
+{
+    public class LocalizationService
+    {
+        private readonly ResourceManager _resourceManager;
+        private readonly ResourceContext _resourceContext;
+
+        public LocalizationService(ResourceManager resourceManager)
+        {
+            _resourceManager = resourceManager;
+            _resourceContext = resourceManager.CreateResourceContext();
+        }
+    }
+}
