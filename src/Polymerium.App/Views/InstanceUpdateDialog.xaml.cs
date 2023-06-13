351d02cd75d22009ad7affbc0bcf47364b0613cb
Chien Zhang <zqy0224@live.com>
2023-06-13 18:06:27 +0800

    为版本升级页面建了个界面

--- src/Polymerium.App/Views/InstanceUpdateDialog.xaml.cs
+++ src/Polymerium.App/Views/InstanceUpdateDialog.xaml.cs
@@ -20,14 +20,51 @@ public sealed partial class InstanceUpdateDialog : CustomDialog
         InstanceModpackReferenceVersionModel version
     )
     {
+        Instance = instance;
+        Modpack = modpack;
+        Version = version;
         ViewModel = App.Current.Provider.GetRequiredService<InstanceUpdateViewModel>();
         InitializeComponent();
     }
 
+
+
+    public bool IsProcessing
+    {
+        get { return (bool)GetValue(IsProcessingProperty); }
+        set { SetValue(IsProcessingProperty, value); }
+    }
+
+    // Using a DependencyProperty as the backing store for IsProcessing.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty IsProcessingProperty =
+        DependencyProperty.Register(nameof(IsProcessing), typeof(bool), typeof(InstanceUpdateDialog), new PropertyMetadata(false));
+
+
+
+    public bool ApplyLocalFileReset
+    {
+        get { return (bool)GetValue(ApplyLocalFileResetProperty); }
+        set { SetValue(ApplyLocalFileResetProperty, value); }
+    }
+
+    // Using a DependencyProperty as the backing store for ApplyLocalFileReset.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty ApplyLocalFileResetProperty =
+        DependencyProperty.Register(nameof(ApplyLocalFileReset), typeof(bool), typeof(InstanceUpdateDialog), new PropertyMetadata(true));
+
+
+
     public InstanceUpdateViewModel ViewModel { get; }
+    public GameInstanceModel Instance { get; }
+    public InstanceModpackReferenceModel Modpack { get; }
+    public InstanceModpackReferenceVersionModel Version { get; }
 
     private void CancelButton_Click(object sender, RoutedEventArgs e)
     {
         Dismiss();
+    }
+
+    private void ConfirmButton_Click(object sender, RoutedEventArgs e)
+    {
+        IsProcessing = true;
     }
 }
