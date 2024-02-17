cf6d4cb96fc7adc3f80866868cdbe549955fe8c8
Chien Zhang <zqy0224@live.com>
2024-02-18 00:39:40 +0800

    怎么jb提交文件就只提交一半啊

---
+++ src/Polymerium.App/Modals/ModalBase.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Input;
+using Microsoft.UI.Xaml.Controls;
+
+namespace Polymerium.App.Modals;
+
+public class ModalBase : UserControl
+{
+    public ICommand DismissCommand { get; internal set; } = null!;
+}
---
+++ src/Polymerium.App/Services/ModalService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Polymerium.App.Modals;
+
+namespace Polymerium.App.Services;
+
+public class ModalService
+{
+    private Action<ModalBase>? popHandler;
+
+    public void SetHandler(Action<ModalBase> handler)
+    {
+        popHandler = handler;
+    }
+
+    public void Pop<T>(T modal)
+        where T : ModalBase
+    {
+        popHandler?.Invoke(modal);
+    }
+}
