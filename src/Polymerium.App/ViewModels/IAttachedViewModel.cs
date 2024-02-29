ff95684655109c9505849efc6ac851221b464d7e
Chien Zhang <zqy0224@live.com>
2024-02-29 20:21:31 +0800

    让ViewModel也能收Message

---
+++ src/Polymerium.App/ViewModels/IAttachedViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Polymerium.App.ViewModels
+{
+    public interface IAttachedViewModel
+    {
+        public bool OnAttached(object? parameter)
+        {
+            return true;
+        }
+
+        public void OnDetached()
+        {
+        }
+    }
+}
---
+++ src/Polymerium.App/ViewModels/RecipientViewModelBase.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Polymerium.App.ViewModels
+{
+    public abstract class RecipientViewModelBase : ObservableRecipient, IAttachedViewModel
+    {
+        public virtual bool OnAttached(object? parameter)
+        {
+            return true;
+        }
+
+        public virtual void OnDetached()
+        {
+        }
+    }
+}
