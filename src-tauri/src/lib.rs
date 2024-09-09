de69b85a8cd5ae4bb6e4aaec010bd7ed93d2195a
Dearain <d3ara1n@outlook.com>
2024-09-09 16:36:07 +0800

    add vibrancy

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,31 @@
+use std::error::Error;
+
+use tauri::{App, Manager};
+use window_vibrancy::{apply_mica, apply_vibrancy, NSVisualEffectMaterial};
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    #[cfg(debug_assertions)]
+    let builder = tauri::Builder::default().plugin(tauri_plugin_devtools::init());
+    #[cfg(not(debug_assertions))]
+    let builder = tauri::Builder::default();
+
+    builder
+        .setup(setup)
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+fn setup(app: &mut App) -> Result<(), Box<dyn Error>> {
+    let window = app.get_webview_window("main").unwrap();
+
+    #[cfg(target_os = "macos")]
+    apply_vibrancy(&window, NSVisualEffectMaterial::HudWindow, None, None)
+        .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
+
+    #[cfg(target_os = "windows")]
+    apply_mica(&window, None)
+        .expect("Unsupported platform! 'apply_mica' is only supported on Windows 11");
+
+    Ok(())
 }
--- src/app.tsx
+++ src/app.tsx
@@ -1,23 +1,15 @@
-import {createSignal} from "solid-js";
+import { createSignal } from "solid-js";
 import "~/index.css";
-import {Button} from "~/components/ui/button";
+import { Button } from "~/components/ui/button";
 
 export default function App() {
     const [count, setCount] = createSignal(0);
 
     return (
         <main>
-            <h1>Hello world!</h1>
-            <Button class="increment" onClick={() => setCount(count() + 1)} type="button">
-                Clicks: {count()}
+            <Button variant="ghost" onClick={() =>  setCount(count() + 1)}>
+                {count()}
             </Button>
-            <p>
-                Visit{" "}
-                <a href="https://start.solidjs.com" target="_blank">
-                    start.solidjs.com
-                </a>{" "}
-                to learn how to build SolidStart apps.
-            </p>
         </main>
     );
 }
