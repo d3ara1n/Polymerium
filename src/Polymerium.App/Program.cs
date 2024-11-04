df9c23e617b740d8bdff66812d21503131854598
Chien Zhang <d3ara1n@outlook.com>
2024-11-04 11:16:34 +0800

    maximize&minimize

--- src/Polymerium.App/Program.cs
+++ src/Polymerium.App/Program.cs
@@ -4,8 +4,23 @@ using Microsoft.Extensions.Hosting;
 
 namespace Polymerium.App;
 
-class Program
+internal static class Program
 {
+#if DEBUG
+    // Initialization code. Don't use any Avalonia, third-party APIs or any
+    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    // yet and stuff might break.
+    [STAThread]
+    public static void Main(string[] args) => BuildAvaloniaApp()
+        .StartWithClassicDesktopLifetime(args);
+
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace();
+#else
     public static void Main(string[] args)
     {
         var builder = Host.CreateApplicationBuilder();
@@ -13,4 +28,5 @@ class Program
         var host = builder.Build();
         host.Run();
     }
+#endif
 }
