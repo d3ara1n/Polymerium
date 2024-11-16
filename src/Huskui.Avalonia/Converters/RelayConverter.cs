e6b9cfe5053c659be65236dac7f93eb34a0a846e
Dearain <d3ara1n@outlook.com>
2024-11-16 16:59:47 +0800

    update TridentV2 README.md

--- src/Huskui.Avalonia/Converters/RelayConverter.cs
+++ src/Huskui.Avalonia/Converters/RelayConverter.cs
@@ -5,8 +5,10 @@ namespace Huskui.Avalonia.Converters;
 
 public class RelayConverter(Func<object?, object?, object?> convert) : IValueConverter
 {
-    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) =>
-        convert(value, parameter);
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return convert(value, parameter);
+    }
 
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
--- src/Huskui.Avalonia/Properties/AssemblyInfo.cs
+++ src/Huskui.Avalonia/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
-﻿using System.Reflection;
-using Avalonia.Metadata;
+﻿using Avalonia.Metadata;
 
 [assembly: XmlnsDefinition("https://github.com/d3ara1n/Huskui.Avalonia", "Huskui.Avalonia.Controls")]
 [assembly: XmlnsDefinition("https://github.com/d3ara1n/Huskui.Avalonia/Converters", "Huskui.Avalonia.Converters")]
