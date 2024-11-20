9d41cf550adadeed42e9af887314358028de9b43
Dearain <d3ara1n@outlook.com>
2024-11-20 19:59:01 +0800

    add TabControl.axaml & TabItem.axaml

--- src/Huskui.Avalonia/Controls/Page.cs
+++ src/Huskui.Avalonia/Controls/Page.cs
@@ -1,4 +1,5 @@
-﻿using Avalonia.Controls;
+﻿using Avalonia;
+using Avalonia.Controls;
 
 namespace Huskui.Avalonia.Controls;
 
--- src/Trident.Abstractions/Repositories/Resources/Version.cs
+++ src/Trident.Abstractions/Repositories/Resources/Version.cs
@@ -1,3 +1,13 @@
 ﻿namespace Trident.Abstractions.Repositories.Resources;
 
-public record Version();
+public record Version(
+    string Label,
+    string Namespace,
+    string ProjectId,
+    string VersionId,
+    string VersionName,
+    ReleaseType ReleaseType,
+    DateTimeOffset PublishedAt,
+    uint DownloadCount,
+    string Changelog
+);
