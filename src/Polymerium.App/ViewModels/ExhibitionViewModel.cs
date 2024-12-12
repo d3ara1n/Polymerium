d1f004ac07f22c47f05c91f5de5cae7aea8c325e
Chien Zhang <d3ara1n@outlook.com>
2024-12-13 02:56:51 +0800

    Frame可以跑出History Stack

--- src/Polymerium.App/ViewModels/ExhibitionViewModel.cs
+++ src/Polymerium.App/ViewModels/ExhibitionViewModel.cs
@@ -1,18 +1,9 @@
-﻿using System;
-using System.Threading;
-using System.Threading.Tasks;
-using Avalonia.Threading;
-using Polymerium.App.Facilities;
+﻿using Polymerium.App.Facilities;
 using Polymerium.Trident.Services;
 
 namespace Polymerium.App.ViewModels;
 
-public class ExhibitionViewModel(ViewBag bag, ProfileService profileService) : ViewModelBase
+public class ExhibitionViewModel(ProfileService profileService) : ViewModelBase
 {
-    public string Title { get; } = $"Exhibition({bag.Parameter ?? "None"})";
 
-    protected override Task OnInitializedAsync(Dispatcher dispatcher, CancellationToken token)
-    {
-        return Task.Delay(TimeSpan.FromSeconds(5), token);
-    }
 }
---
+++ src/Polymerium.App/ViewModels/UnknownViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Avalonia.Threading;
+using Polymerium.App.Facilities;
+
+namespace Polymerium.App.ViewModels;
+
+public class UnknownViewModel(ViewBag bag) : ViewModelBase
+{
+    public string Title { get; } = $"User's Unknown Playground({bag.Parameter ?? "None"})";
+
+    protected override Task OnInitializedAsync(Dispatcher dispatcher, CancellationToken token)
+    {
+        return Task.Delay(TimeSpan.FromSeconds(5), token);
+    }
+}
