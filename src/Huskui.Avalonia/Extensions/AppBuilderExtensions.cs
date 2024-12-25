c6fd9c6535dd7e52ef7d1654b2d38e9648d5bdc0
Chien Zhang <d3ara1n@outlook.com>
2024-12-25 21:23:34 +0800

    确定最终字体

--- src/Huskui.Avalonia/Extensions/AppBuilderExtensions.cs
+++ src/Huskui.Avalonia/Extensions/AppBuilderExtensions.cs
@@ -10,12 +10,10 @@ public static class AppBuilderExtensions
     {
         return appBuilder.ConfigureFonts(fontManager =>
         {
-            fontManager.AddFontCollection(new EmbeddedFontCollection(new Uri("fonts:MaoKenWangXingYuan"),
-                new Uri("avares://Huskui.Avalonia/Assets/Fonts/MaoKenWangXingYuan")));
-            fontManager.AddFontCollection(new EmbeddedFontCollection(new Uri("fonts:Outfit"),
-                new Uri("avares://Huskui.Avalonia/Assets/Fonts/Outfit")));
             fontManager.AddFontCollection(new EmbeddedFontCollection(new Uri("fonts:Quicksand"),
                 new Uri("avares://Huskui.Avalonia/Assets/Fonts/Quicksand")));
+            fontManager.AddFontCollection(new EmbeddedFontCollection(new Uri("fonts:MaoKenWangXingYuan"),
+                new Uri("avares://Huskui.Avalonia/Assets/Fonts/MaoKenWangXingYuan")));
         }).With(new FontManagerOptions
         {
             DefaultFamilyName = "fonts:Quicksand#Quicksand",
