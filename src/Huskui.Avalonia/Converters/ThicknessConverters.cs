65fa519b9a5725b11b5d5c448b6767c829fce557
Dearain <d3ara1n@outlook.com>
2025-02-03 16:23:05 +0800

    add Modal.axaml & SplitButton.axaml

--- src/Huskui.Avalonia/Converters/ThicknessConverters.cs
+++ src/Huskui.Avalonia/Converters/ThicknessConverters.cs
@@ -5,6 +5,30 @@ namespace Huskui.Avalonia.Converters;
 
 public static class ThicknessConverters
 {
+    public static IValueConverter WithoutBottom { get; } = new RelayConverter((v, _) => v switch
+    {
+        Thickness it => new Thickness(it.Left, it.Top, it.Right, 0d),
+        _ => v
+    });
+
+    public static IValueConverter WithoutTop { get; } = new RelayConverter((v, _) => v switch
+    {
+        Thickness it => new Thickness(it.Left, 0d, it.Right, it.Bottom),
+        _ => v
+    });
+
+    public static IValueConverter WithoutLeft { get; } = new RelayConverter((v, _) => v switch
+    {
+        Thickness it => new Thickness(0d, it.Top, it.Right, it.Left),
+        _ => v
+    });
+
+    public static IValueConverter WithoutRight { get; } = new RelayConverter((v, _) => v switch
+    {
+        Thickness it => new Thickness(it.Left, it.Top, 0d, it.Bottom),
+        _ => v
+    });
+
     public static IValueConverter ToDouble { get; } = new RelayConverter((v, p) =>
     {
         if (v is Thickness thickness)
