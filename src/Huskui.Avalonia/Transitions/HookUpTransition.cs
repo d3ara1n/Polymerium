4e8155a6d297d74bdb2fa798350397ebf7a0c87b
Dearain <d3ara1n@outlook.com>
2025-02-09 23:58:23 +0800

    add dialog simple transition

---
+++ src/Huskui.Avalonia/Transitions/HookUpTransition.cs
@@ -0,0 +1,32 @@
+﻿using Avalonia;
+using Avalonia.Animation.Easings;
+using Avalonia.Media;
+
+namespace Huskui.Avalonia.Transitions;
+
+public class HookUpTransition : PageTransitionBase
+{
+    protected override void Configure(Builder from, Builder to, Lazy<Visual> parentAccessor)
+    {
+        var height = 72d;
+        from.Animation(new SineEaseOut())
+            .AddFrame(0d, [
+                (TranslateTransform.YProperty, 0),
+                (Visual.OpacityProperty, 1d)
+            ])
+            .AddFrame(1d, [
+                (TranslateTransform.YProperty, height),
+                (Visual.OpacityProperty, 0d)
+            ]);
+
+        to.Animation(new CubicEaseOut())
+            .AddFrame(0d, [
+                (TranslateTransform.YProperty, height),
+                (Visual.OpacityProperty, 0d)
+            ])
+            .AddFrame(1d, [
+                (TranslateTransform.YProperty, 0),
+                (Visual.OpacityProperty, 1d)
+            ]);
+    }
+}
