cf88a176658fda4478bfb2a5cf382a5b28e6aca6
Chien Zhang <d3ara1n@outlook.com>
2025-02-21 15:19:55 +0800

    add LazyObject.cs

--- src/Huskui.Avalonia/Controls/SkeletonControl.cs
+++ src/Huskui.Avalonia/Controls/SkeletonControl.cs
@@ -4,14 +4,14 @@ using Avalonia.Controls.Metadata;
 
 namespace Huskui.Avalonia.Controls;
 
-[PseudoClasses(":loading")]
+[PseudoClasses(":loading", ":animated")]
 public class SkeletonControl : ContentControl
 {
     public static readonly DirectProperty<SkeletonControl, bool> IsLoadingProperty =
         AvaloniaProperty.RegisterDirect<SkeletonControl, bool>(nameof(IsLoading), o => o.IsLoading,
             (o, v) => o.IsLoading = v);
 
-    private bool _isLoading;
+    private bool _isLoading = true;
 
     public bool IsLoading
     {
@@ -20,6 +20,21 @@ public class SkeletonControl : ContentControl
         {
             if (SetAndRaise(IsLoadingProperty, ref _isLoading, value))
                 PseudoClasses.Set(":loading", value);
+        }
+    }
+
+    public static readonly DirectProperty<SkeletonControl, bool> IsAnimatedProperty =
+        AvaloniaProperty.RegisterDirect<SkeletonControl, bool>(nameof(IsAnimated), o => o.IsAnimated,
+            (o, v) => o.IsAnimated = v);
+
+    private bool _isAnimated = true;
+
+    public bool IsAnimated
+    {
+        get => _isAnimated;
+        set
+        {
+            if (SetAndRaise(IsAnimatedProperty, ref _isAnimated, value)) PseudoClasses.Set(":animated", value);
         }
     }
 }
