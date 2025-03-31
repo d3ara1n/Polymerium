4a550e1dc474cb89beae1a110962f1dd5163435a
Dearain <d3ara1n@outlook.com>
2025-03-31 11:33:14 +0800

    add ConstrainedBox.axaml

---
+++ src/Huskui.Avalonia/Controls/ConstrainedBox.cs
@@ -0,0 +1,31 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Layout;
+
+namespace Huskui.Avalonia.Controls;
+
+public class ConstrainedBox : ContentControl
+{
+    public static readonly StyledProperty<double> AspectRatioProperty =
+        AvaloniaProperty.Register<ConstrainedBox, double>(nameof(AspectRatio), defaultValue: 1.0);
+
+    public double AspectRatio
+    {
+        get => GetValue(AspectRatioProperty);
+        set => SetValue(AspectRatioProperty, value);
+    }
+
+    protected override Size MeasureOverride(Size availableSize)
+    {
+        var width = availableSize.Width;
+        var height = width / AspectRatio;
+
+        if (height > availableSize.Height)
+        {
+            height = availableSize.Height;
+            width = height * AspectRatio;
+        }
+
+        return new Size(width, height);
+    }
+}
