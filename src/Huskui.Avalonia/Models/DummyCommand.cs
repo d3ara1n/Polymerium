44ac3622825f8c489788e734e5bb4b16849d2b02
Dearain <d3ara1n@outlook.com>
2025-04-03 00:15:12 +0800

    添加一个DummyCommand

---
+++ src/Huskui.Avalonia/Models/DummyCommand.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Input;
+
+namespace Huskui.Avalonia.Models;
+
+internal class DummyCommand : ICommand
+{
+    public static readonly DummyCommand Instance = new();
+    public bool CanExecute(object? parameter) => true;
+
+    public void Execute(object? parameter) { }
+    public event EventHandler? CanExecuteChanged;
+}
--- src/Huskui.Avalonia/Models/NotificationAction.cs
+++ src/Huskui.Avalonia/Models/NotificationAction.cs
@@ -4,10 +4,7 @@ namespace Huskui.Avalonia.Models;
 
 public class NotificationAction(string text, ICommand command, object? parameter = null)
 {
-    public NotificationAction() : this("_", null!)
-    {
-        // TODO: Set to DismissCommand
-    }
+    public NotificationAction() : this("_", DummyCommand.Instance) { }
 
     public string Text { get; set; } = text;
     public ICommand Command { get; set; } = command;
