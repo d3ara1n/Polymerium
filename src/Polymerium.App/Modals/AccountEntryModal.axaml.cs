e19f68c7009cd28d8b1434f980c9d232a5dd0548
Dearain <d3ara1n@outlook.com>
2025-04-30 15:56:51 +0800

    feat(modals): 添加账户相关模态窗口
    - 在 `AccountsView.axaml` 中添加了 "Add Account" 按钮
    - 新增 `AccountEntryModal.axaml` 和 `AccountEntryModal.axaml.cs` 文件，提供账户输入功能
    - 新增 `AccountCreationModal.axaml` 和 `AccountCreationModal.axaml.cs` 文件，提供账户创建功能
    - 更新了 `UnknownViewModel.cs` 文件以支持打开 `AccountEntryModal`
    - 调整了 `Page.axaml` 中的文本样式以提升视觉效果
    - 修改 `ExhibitPackageModal.axaml` 和 `MarketplacePortalView.axaml` 中的布局属性以适应新功能

---
+++ src/Polymerium.App/Modals/AccountEntryModal.axaml.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using Huskui.Avalonia.Controls;
+
+namespace Polymerium.App.Modals;
+
+public partial class AccountEntryModal : Modal
+{
+    public AccountEntryModal()
+    {
+        InitializeComponent();
+    }
+
+    #region Commands
+
+    [RelayCommand]
+    private void Dismiss()
+    {
+        RaiseEvent(new OverlayItem.DismissRequestedEventArgs(this));
+    }
+
+    #endregion
+}
