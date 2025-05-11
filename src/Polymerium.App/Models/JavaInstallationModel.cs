4588136e17769dc16467993e9308ff32eb1e3934
Dearain <d3ara1n@outlook.com>
2025-05-11 19:38:24 +0800

    feat(JavaHomeContainer): 更新Java安装模型的显示和数据绑定
    - 修改`JavaHomeContainer.axaml`，引入`PlaceholderPresenter`以处理可能为空的值
    - 更新`JavaInstallationModel.cs`中的`major`类型从`uint?`改为`int?`以更好地匹配数据类型
    - 使版本信息更加灵活，支持未知供应商和版本的显示

--- src/Polymerium.App/Models/JavaInstallationModel.cs
+++ src/Polymerium.App/Models/JavaInstallationModel.cs
@@ -2,14 +2,14 @@
 
 namespace Polymerium.App.Models;
 
-public class JavaInstallationModel(string path, string? vendor, string? version, uint? major) : ModelBase
+public class JavaInstallationModel(string path, string? vendor, string? version, int? major) : ModelBase
 {
     #region Direct
 
     public string Path => path;
     public string? Vendor => vendor;
     public string? Version => version;
-    public uint? Major => major;
+    public int? Major => major;
 
     #endregion
 }
