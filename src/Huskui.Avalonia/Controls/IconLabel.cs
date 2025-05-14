97c090f4447397183fcea65edf51c495f67b8532
Dearain <d3ara1n@outlook.com>
2025-05-14 22:38:35 +0800

    feat(controls): 添加 IconLabel 控件
    - 新增 `IconLabel.axaml` 文件，定义控件的 XAML 结构
    - 新增 `IconLabel.cs` 文件，实现控件的逻辑和属性
    - 更新 `Controls.axaml` 文件，包含新的 `IconLabel` 控件资源

---
+++ src/Huskui.Avalonia/Controls/IconLabel.cs
@@ -0,0 +1,45 @@
+﻿using Avalonia;
+using Avalonia.Controls.Primitives;
+using FluentIcons.Common;
+
+namespace Huskui.Avalonia.Controls;
+
+public class IconLabel : TemplatedControl
+{
+    public static readonly StyledProperty<Symbol> IconProperty =
+        AvaloniaProperty.Register<IconLabel, Symbol>(nameof(Icon));
+
+    public Symbol Icon
+    {
+        get => GetValue(IconProperty);
+        set => SetValue(IconProperty, value);
+    }
+
+    public static readonly StyledProperty<string> TextProperty =
+        AvaloniaProperty.Register<IconLabel, string>(nameof(Text));
+
+    public string Text
+    {
+        get => GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+
+    public static readonly StyledProperty<IconVariant> VariantProperty =
+        AvaloniaProperty.Register<IconLabel, IconVariant>(nameof(Variant));
+
+    public IconVariant Variant
+    {
+        get => GetValue(VariantProperty);
+        set => SetValue(VariantProperty, value);
+    }
+
+
+    public static readonly StyledProperty<double> SpacingProperty =
+        AvaloniaProperty.Register<IconLabel, double>(nameof(Spacing), 4d);
+
+    public double Spacing
+    {
+        get => GetValue(SpacingProperty);
+        set => SetValue(SpacingProperty, value);
+    }
+}
