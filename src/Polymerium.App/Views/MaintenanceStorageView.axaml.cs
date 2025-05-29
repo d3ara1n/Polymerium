46831e7f6d57edda2f6690b5850e372ba5ccef85
Dearain <d3ara1n@outlook.com>
2025-05-29 11:53:17 +0800

    feat(views): 添加维护存储视图和自定义控件
    - 新增 `MaintenanceStorageView.axaml` 和 `MaintenanceStorageView.axaml.cs` 文件，定义存储维护页面的布局和逻辑
    - 新增自定义控件 `Plaque.axaml` 和 `Plaque.cs`，用于实现自定义样式和模板

---
+++ src/Polymerium.App/Views/MaintenanceStorageView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Huskui.Avalonia.Controls;
+
+namespace Polymerium.App.Views;
+
+public partial class MaintenanceStorageView : Page
+{
+    public MaintenanceStorageView()
+    {
+        InitializeComponent();
+    }
+}
