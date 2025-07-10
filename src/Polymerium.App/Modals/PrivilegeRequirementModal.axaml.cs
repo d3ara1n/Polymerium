aa09293547a8f5c3ceaa0aa77aac63f786435f10
Dearain <d3ara1n@outlook.com>
2025-07-10 19:41:58 +0800

    fix(ViewModels): 修复进度计算逻辑
    - 修改了 `InstanceSetupViewModel.cs` 中的进度计算公式，以确保进度正确显示
    - 修正了 `PrivilegeRequirementModal.axaml.cs` 中的代码格式，使其更加整洁

--- src/Polymerium.App/Modals/PrivilegeRequirementModal.axaml.cs
+++ src/Polymerium.App/Modals/PrivilegeRequirementModal.axaml.cs
@@ -2,7 +2,7 @@
 
 namespace Polymerium.App.Modals;
 
-public partial class PrivilegeRequirementModal : Modal
+public partial  class PrivilegeRequirementModal : Modal
 {
     public PrivilegeRequirementModal()
     {
