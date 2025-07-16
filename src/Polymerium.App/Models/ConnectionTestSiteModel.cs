4f010d17c4614f64c415a8ff1d1d9760afc5245f
Dearain <d3ara1n@outlook.com>
2025-07-16 16:34:37 +0800

    feat(widget): 添加连接测试小部件功能
    - 新增 `ConnectionTestSiteModel` 类，用于表示连接测试站点
    - 在 `ConnectionTestWidget.axaml` 中实现了 `ItemsControl` 以显示多个连接测试结果
    - 通过 `ObservableCollection` 管理连接测试站点的集合
    - 添加了 `Perform` 命令以触发连接测试操作
    - 优化了界面布局，使用 `StackPanel` 和 `ItemsControl` 提升用户体验

---
+++ src/Polymerium.App/Models/ConnectionTestSiteModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Polymerium.App.Models;
+
+public partial class ConnectionTestSiteModel(string display, Uri endpoint) : ObservableObject
+{
+    #region Direct
+
+    public string Display => display;
+    public Uri Endpoint => endpoint;
+
+    #endregion
+
+    #region Reactive
+
+    [ObservableProperty]
+    public partial double Latency { get; set; }
+
+    [ObservableProperty]
+    public partial bool IsTesting { get; set; }
+
+    #endregion
+}
--- src/Polymerium.App/Widgets/ConnectionTestWidget.axaml.cs
+++ src/Polymerium.App/Widgets/ConnectionTestWidget.axaml.cs
@@ -1,3 +1,21 @@
-﻿namespace Polymerium.App.Widgets;
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.Input;
+using Polymerium.App.Models;
 
-public class ConnectionTestWidget : WidgetBase { }
+namespace Polymerium.App.Widgets;
+
+public partial class ConnectionTestWidget : WidgetBase
+{
+    #region Direct
+
+    public ObservableCollection<ConnectionTestSiteModel> Sites { get; } = new();
+
+    #endregion
+
+    #region Commands
+
+    [RelayCommand]
+    private void Perform() { }
+
+    #endregion
+}
