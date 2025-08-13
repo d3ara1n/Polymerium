7c824071678e4c910e40738407642fe0cd838791
d3ara1n <d3ara1n@outlook.com>
2025-08-13 16:04:36 +0800

    feat: 添加实例资源视图
    - 添加了实例资源视图，包括截图、存档和服务器选项卡
    - 在`InstanceViewModel.cs`中注册了`InstanceAssetsView`
    - 创建了`InstanceAssetsView.axaml`、`InstanceAssetsView.axaml.cs`和`InstanceAssetsViewModel.cs`文件
    - 允许用户管理实例的截图、存档和服务器

---
+++ src/Polymerium.App/Views/InstanceAssetsView.axaml.cs
@@ -0,0 +1,9 @@
+﻿using Polymerium.App.Controls;
+
+namespace Polymerium.App.Views
+{
+    public class InstanceAssetsView : Subpage
+    {
+        public InstanceAssetsView() => InitializeComponent();
+    }
+}
