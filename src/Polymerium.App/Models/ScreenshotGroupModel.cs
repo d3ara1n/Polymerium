79677667e1361ef979a865e95684d7558f225035
d3ara1n <d3ara1n@outlook.com>
2025-08-13 17:25:45 +0800

    feat: 实现截图浏览功能
    - 添加了 `ScreenshotModel` 和 `ScreenshotGroupModel` 用于表示截图数据
    - 在 `InstanceAssetsView` 中实现了截图分组和展示的UI
    - `InstanceAssetsViewModel` 负责加载和组织截图数据
    - 修改了 `InstanceAssetsView.axaml` 以绑定 `InstanceAssetsViewModel` 并显示截图
    - 修改了 `PackageContainer.axaml` and `InstanceHomeView.axaml` 以优化主题颜色显示.

---
+++ src/Polymerium.App/Models/ScreenshotGroupModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Polymerium.App.Facilities;
+
+namespace Polymerium.App.Models
+{
+    public class ScreenshotGroupModel(DateTimeOffset date) : ModelBase
+    {
+        #region Direct
+
+        public DateTimeOffset DateRaw => date;
+        public string Date => DateRaw.ToString("D");
+
+        #endregion
+
+        #region Reactive
+
+        public ObservableCollection<ScreenshotModel> Screenshots { get; } = [];
+
+        #endregion
+    }
+}
--- src/Polymerium.App/ViewModels/InstanceAssetsViewModel.cs
+++ src/Polymerium.App/ViewModels/InstanceAssetsViewModel.cs
@@ -1,8 +1,58 @@
-﻿using Polymerium.App.Facilities;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Polymerium.App.Facilities;
+using Polymerium.App.Models;
 using Polymerium.Trident.Services;
+using Trident.Abstractions;
 
 namespace Polymerium.App.ViewModels
 {
     public class InstanceAssetsViewModel(ViewBag bag, InstanceManager instanceManager, ProfileManager profileManager)
-        : InstanceViewModelBase(bag, instanceManager, profileManager) { }
+        : InstanceViewModelBase(bag, instanceManager, profileManager)
+    {
+        #region Reactive
+
+        public ObservableCollection<ScreenshotGroupModel> Groups { get; } = [];
+
+        #endregion
+
+        #region Overrides
+
+        protected override Task OnInitializeAsync(CancellationToken token) => Task.Run(LoadAsync, token);
+
+        #endregion
+
+        #region Other
+
+        private Task LoadAsync()
+        {
+            var dir = new DirectoryInfo(Path.Combine(PathDef.Default.DirectoryOfBuild(Basic.Key), "screenshots"));
+            if (!dir.Exists)
+            {
+                return Task.CompletedTask;
+            }
+
+            foreach (var files in dir
+                                 .GetFiles("*.png", SearchOption.TopDirectoryOnly)
+                                 .GroupBy(x => x.CreationTimeUtc.Date))
+            {
+                var group = new ScreenshotGroupModel(files.Key);
+                foreach (var file in files)
+                {
+                    group.Screenshots.Add(new ScreenshotModel(new Uri(file.FullName, UriKind.Absolute),
+                                                              file.CreationTimeUtc));
+                }
+
+                Groups.Add(group);
+            }
+
+            return Task.CompletedTask;
+        }
+
+        #endregion
+    }
 }
