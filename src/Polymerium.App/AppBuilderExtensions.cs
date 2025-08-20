bd5380790f3ce3836225b42310be490f6ead92e7
d3ara1n <d3ara1n@outlook.com>
2025-08-20 14:18:02 +0800

    refactor(fonts): 替换字体 `AidianFengYaHei` 为 `YSHaoShenTi`
    - 将字体 `AidianFengYaHei` 替换为 `YSHaoShenTi`，并更新了相应的资源路径和默认字体回退设置。
    - 原因是原字体可能存在版权问题或者其他兼容性问题，更换为新的字体以保证应用正常使用。
    - 删除了 `OFL.txt` 文件，因为不再需要它了。

--- src/Polymerium.App/AppBuilderExtensions.cs
+++ src/Polymerium.App/AppBuilderExtensions.cs
@@ -13,13 +13,13 @@ namespace Polymerium.App
                 {
                     fontManager.AddFontCollection(new EmbeddedFontCollection(new("fonts:Manrope"),
                                                                              new("avares://Polymerium.App/Assets/Fonts/Manrope")));
-                    fontManager.AddFontCollection(new EmbeddedFontCollection(new("fonts:AidianFengYaHei"),
-                                                                             new("avares://Polymerium.App/Assets/Fonts/AidianFengYaHei")));
+                    fontManager.AddFontCollection(new EmbeddedFontCollection(new("fonts:YSHaoShenTi"),
+                                                                             new("avares://Polymerium.App/Assets/Fonts/YSHaoShenTi")));
                 })
                .With(new FontManagerOptions
                 {
                     DefaultFamilyName = "fonts:Manrope#Manrope",
-                    FontFallbacks = [new() { FontFamily = new("fonts:AidianFengYaHei#爱点风雅黑") }]
+                    FontFallbacks = [new() { FontFamily = new("fonts:YSHaoShenTi#YOUSHEhaoshenti") }]
                 });
             return appBuilder;
         }
