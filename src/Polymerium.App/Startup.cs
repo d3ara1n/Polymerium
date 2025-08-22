7a7e1e296511dde232cae9aa0318633f0e3ccdd7
Dearain <d3ara1n@outlook.com>
2025-08-22 17:14:56 +0800

    feat: 添加 CurseForge 和 Modrinth 导入器
    - 将 `CurseForgeImporter` 和 `ModrinthImporter` 添加到 `IProfileImporter` 的依赖注入容器中。
    - 允许应用导入 CurseForge 和 Modrinth 配置文件。

--- src/Polymerium.App/Startup.cs
+++ src/Polymerium.App/Startup.cs
@@ -9,8 +9,10 @@ using NeoSmart.Caching.Sqlite;
 using Polly;
 using Polymerium.App.Services;
 using Polymerium.Trident.Extensions;
+using Polymerium.Trident.Importers;
 using Polymerium.Trident.Services;
 using Trident.Abstractions;
+using Trident.Abstractions.Importers;
 using Velopack;
 using Velopack.Sources;
 
@@ -18,69 +20,69 @@ namespace Polymerium.App;
 
 public static class Startup
 {
-    public static void ConfigureServices(
-        IServiceCollection services,
-        IConfiguration _,
-        IHostEnvironment environment)
+    public static void ConfigureServices(IServiceCollection services, IConfiguration _, IHostEnvironment environment)
     {
         services
-            .AddAvalonia()
-            .AddHttpClient()
-            .ConfigureHttpClientDefaults(builder => builder
-                .RemoveAllLoggers()
-                .ConfigureHttpClient(client => client.DefaultRequestHeaders
-                    .UserAgent
-                    .Add(new(Program.Brand, Program.Version)))
-                .AddTransientHttpErrorPolicy(configure => configure.RetryAsync()))
-            .AddLogging(logging => logging
-                .AddConsole()
-                .AddDebug()
-                .AddFilter<ConsoleLoggerProvider>(null,
-                    environment.EnvironmentName == "Development"
-                        ? LogLevel.Debug
-                        : LogLevel.Information)
-                .AddFilter<DebugLoggerProvider>(null, LogLevel.Trace))
-            .AddSqliteCache(setup =>
+           .AddAvalonia()
+           .AddHttpClient()
+           .ConfigureHttpClientDefaults(builder => builder
+                                                  .RemoveAllLoggers()
+                                                  .ConfigureHttpClient(client =>
+                                                                           client.DefaultRequestHeaders.UserAgent
+                                                                              .Add(new(Program.Brand, Program.Version)))
+                                                  .AddTransientHttpErrorPolicy(configure => configure.RetryAsync()))
+           .AddLogging(logging => logging
+                                 .AddConsole()
+                                 .AddDebug()
+                                 .AddFilter<ConsoleLoggerProvider>(null,
+                                                                   environment.EnvironmentName == "Development"
+                                                                       ? LogLevel.Debug
+                                                                       : LogLevel.Information)
+                                 .AddFilter<DebugLoggerProvider>(null, LogLevel.Trace))
+           .AddSqliteCache(setup =>
             {
                 setup.MemoryOnly = false;
                 var dir = PathDef.Default.PrivateDirectory(Program.Brand);
                 var path = Path.Combine(dir, "cache.sqlite.db");
-                if (!Directory.Exists(dir)) Directory.CreateDirectory(dir);
+                if (!Directory.Exists(dir))
+                    Directory.CreateDirectory(dir);
 
                 setup.CachePath = path;
             })
-            .AddMemoryCache();
+           .AddMemoryCache();
 
         // Trident
         services
-            .AddPrismLauncher()
-            .AddMojangLauncher()
-            .AddMicrosoft()
-            .AddXboxLive()
-            .AddMinecraft()
-            .AddSingleton<ProfileManager>()
-            .AddSingleton<RepositoryAgent>()
-            .AddSingleton<ImporterAgent>()
-            .AddSingleton<InstanceManager>();
+           .AddTransient<IProfileImporter, CurseForgeImporter>()
+           .AddTransient<IProfileImporter, ModrinthImporter>()
+           .AddPrismLauncher()
+           .AddMojangLauncher()
+           .AddMicrosoft()
+           .AddXboxLive()
+           .AddMinecraft()
+           .AddSingleton<ProfileManager>()
+           .AddSingleton<RepositoryAgent>()
+           .AddSingleton<ImporterAgent>()
+           .AddSingleton<InstanceManager>();
 
 
         // App
         services
-            .AddViewFacilities()
-            .AddFreeSql()
-            .AddTransient<IRepositoryProviderAccessor, BuiltinRepositoryProviderAccessor>()
-            .AddTransient<IRepositoryProviderAccessor, UserRepositoryProviderAccessor>()
-            .AddSingleton<ConfigurationService>()
-            .AddSingleton<NotificationService>()
-            .AddSingleton<NavigationService>()
-            .AddSingleton<OverlayService>()
-            .AddSingleton<DataService>()
-            .AddSingleton<PersistenceService>()
-            .AddSingleton<ScrapService>()
-            .AddSingleton<InstanceService>()
-            .AddSingleton<WidgetHostService>()
-            .AddSingleton<UpdateManager>(_ => new(new GithubSource("https://github.com/d3ara1n/Polymerium",
-                null,
-                true)));
+           .AddViewFacilities()
+           .AddFreeSql()
+           .AddTransient<IRepositoryProviderAccessor, BuiltinRepositoryProviderAccessor>()
+           .AddTransient<IRepositoryProviderAccessor, UserRepositoryProviderAccessor>()
+           .AddSingleton<ConfigurationService>()
+           .AddSingleton<NotificationService>()
+           .AddSingleton<NavigationService>()
+           .AddSingleton<OverlayService>()
+           .AddSingleton<DataService>()
+           .AddSingleton<PersistenceService>()
+           .AddSingleton<ScrapService>()
+           .AddSingleton<InstanceService>()
+           .AddSingleton<WidgetHostService>()
+           .AddSingleton<UpdateManager>(_ => new(new GithubSource("https://github.com/d3ara1n/Polymerium",
+                                                                  null,
+                                                                  true)));
     }
 }
