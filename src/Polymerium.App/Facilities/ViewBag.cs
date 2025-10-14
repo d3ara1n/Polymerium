2399f568a7dbe8cca183c2c251531a60120a55cf
Dearain <d3ara1n@outlook.com>
2025-10-14 13:34:57 +0800

    refactor: 优化 `ViewBag` 并改进 `ViewModelBase` 文档
    - 将 `ViewBag` 构造函数的 `_parameter` 字段重命名为 `field`，以提高可读性。
    - 修正了 `ViewModelBase` 中关于后台任务的文档，使用了 `<see cref="Task.Run" />` 标签以便更好地链接到 `Task.Run`。

--- src/Polymerium.App/Facilities/ViewBag.cs
+++ src/Polymerium.App/Facilities/ViewBag.cs
@@ -3,13 +3,12 @@ namespace Polymerium.App.Facilities;
 public class ViewBag
 {
     private readonly ViewBagFactory? _factory;
-    private readonly object? _parameter;
 
     public ViewBag(ViewBagFactory factory) => _factory = factory;
 
-    public ViewBag(object? parameter) => _parameter = parameter;
+    public ViewBag(object? parameter) => Parameter = parameter;
 
-    public object? Parameter => _parameter ?? _factory?.Bag;
+    public object? Parameter => field ?? _factory?.Bag;
 
     public bool IsEmpty => Parameter is null;
 }
--- src/Polymerium.App/Facilities/ViewModelBase.cs
+++ src/Polymerium.App/Facilities/ViewModelBase.cs
@@ -18,7 +18,7 @@ public abstract class ViewModelBase : ObservableObject, IPageModel
     #endregion
 
     /// <summary>
-    ///     该函数跑在 UI 线程 上，如果需要后台需要 Task.Run
+    ///     该函数跑在 UI 线程 上，如果需要后台需要 <see cref="Task.Run" />
     /// </summary>
     /// <param name="token"></param>
     /// <returns></returns>
