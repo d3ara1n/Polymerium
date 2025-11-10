0f408b2366b0128c2d65dcfa99d974356e176be6
Dearain <d3ara1n@outlook.com>
2025-11-10 15:57:49 +0800

    chore: config sentry more detailed

--- src/Polymerium.App/Services/SentryHostedService.cs
+++ src/Polymerium.App/Services/SentryHostedService.cs
@@ -7,15 +7,27 @@ using Sentry;
 
 namespace Polymerium.App.Services;
 
-public class SentryHostedService : IHostedService
+public class SentryHostedService(IHostEnvironment environment) : IHostedService
 {
     public Task StartAsync(CancellationToken cancellationToken)
     {
         SentrySdk.Init(options =>
         {
             options.Dsn = "https://70f1e791a5f2b8cb31f0947a1bac5e7a@o941379.ingest.us.sentry.io/4510328831410176";
-            options.Release = Program.Debug ? "In Dev" : Program.Version;
             options.AutoSessionTracking = true;
+            options.Environment = environment.EnvironmentName;
+            if (Program.Debug)
+            {
+                options.Release = "In Dev";
+                options.ProfilesSampleRate = 1.0f;
+                options.TracesSampleRate = 1.0f;
+            }
+            else
+            {
+                options.Release = Program.Version;
+                options.ProfilesSampleRate = 0.1f;
+                options.TracesSampleRate = 0.1f;
+            }
             options.SendDefaultPii = true;
         });
 
