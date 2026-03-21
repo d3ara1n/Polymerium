a9f8c6c5c27ca8f76c1275766f5660f185d52658
Dearain <d3ara1n@outlook.com>
2026-03-21 18:38:34 +0800

    refactor: 更换图标包以支持 avalonia 12

--- src/Polymerium.App/Models/InstanceSubpageEntryModel.cs
+++ src/Polymerium.App/Models/InstanceSubpageEntryModel.cs
@@ -1,16 +1,17 @@
 using System;
 using CommunityToolkit.Mvvm.ComponentModel;
+using FluentIcons.Common;
 using IconPacks.Avalonia.Lucide;
 using Polymerium.App.Facilities;
 
 namespace Polymerium.App.Models;
 
-public partial class InstanceSubpageEntryModel(Type page, PackIconLucideKind icon) : ModelBase
+public partial class InstanceSubpageEntryModel(Type page, Symbol icon) : ModelBase
 {
     #region Reactive
 
     [ObservableProperty]
-    public partial PackIconLucideKind Icon { get; set; } = icon;
+    public partial Symbol Icon { get; set; } = icon;
 
     #endregion
 
