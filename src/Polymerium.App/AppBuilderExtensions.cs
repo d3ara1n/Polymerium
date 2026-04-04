982f1111c4a9af55dc8ffb184df7afe643734e13
Dearain <d3ara1n@outlook.com>
2026-04-04 18:08:54 +0800

    chore: delete some 不需要的资源

--- src/Polymerium.App/AppBuilderExtensions.cs
+++ src/Polymerium.App/AppBuilderExtensions.cs
@@ -5,26 +5,7 @@ using Avalonia.Media.Fonts;
 namespace Polymerium.App;
 
 public static class AppBuilderExtensions
-{
-    // public static AppBuilder WithFontSetup(this AppBuilder appBuilder)
-    // {
-    //     appBuilder
-    //        .ConfigureFonts(fontManager =>
-    //         {
-    //             fontManager.AddFontCollection(new EmbeddedFontCollection(new("fonts:Manrope"),
-    //                                                                      new("avares://Polymerium.App/Assets/Fonts/Manrope")));
-    //             fontManager.AddFontCollection(new EmbeddedFontCollection(new("fonts:YSHaoShenTi"),
-    //                                                                      new("avares://Polymerium.App/Assets/Fonts/YSHaoShenTi")));
-    //         })
-    //        .With(new FontManagerOptions
-    //         {
-    //             DefaultFamilyName = "fonts:Manrope#Manrope",
-    //             FontFallbacks = [new() { FontFamily = new("fonts:YSHaoShenTi#YOUSHEhaoshenti") }]
-    //         });
-    //     return appBuilder;
-    // }
-
-    public static AppBuilder WithFontSetup(this AppBuilder appBuilder)
+{    public static AppBuilder WithFontSetup(this AppBuilder appBuilder)
     {
         appBuilder
             .ConfigureFonts(fontManager =>
