ad6502e76586b2a309a2a2ef4a068bcbab73123e
Dearain <d3ara1n@outlook.com>
2026-04-26 18:51:44 +0800

    fix: 修复 ImageLoader 在部分情况下会遗漏部分分支情况的问题

--- src/Polymerium.App/SuppressedImageLoader.cs
+++ src/Polymerium.App/SuppressedImageLoader.cs
@@ -3,13 +3,15 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using AsyncImageLoader.Loaders;
 using Avalonia.Media.Imaging;
+using Avalonia.Platform.Storage;
 
 namespace Polymerium.App;
 
-public class SuppressedImageLoader(HttpClient client)
-    : RamCachedWebImageLoader(client, disposeHttpClient: false)
+public class SuppressedImageLoader(HttpClient client) : RamCachedWebImageLoader(client, disposeHttpClient: false)
 {
-    public override async Task<Bitmap?> ProvideImageAsync(string url)
+    public override Task<Bitmap?> ProvideImageAsync(string url) => ProvideImageAsync(url, null);
+
+    public override async Task<Bitmap?> ProvideImageAsync(string url, IStorageProvider? storageProvider = null)
     {
         try
         {
