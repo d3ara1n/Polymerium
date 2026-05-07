b768e60e7d85b3ed823724a561f7a0faffe0e178
Dearain <d3ara1n@outlook.com>
2026-05-07 22:13:56 +0800

    feat: add more ui to snapshot

---
+++ src/Polymerium.App/Pages/SnapshotCreationPage.axaml.cs
@@ -0,0 +1,16 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Huskui.Avalonia.Controls;
+using Page = Huskui.Avalonia.Controls.Page;
+
+namespace Polymerium.App.Pages;
+
+public partial class SnapshotCreationPage : Page
+{
+    public SnapshotCreationPage()
+    {
+        InitializeComponent();
+    }
+}
+
