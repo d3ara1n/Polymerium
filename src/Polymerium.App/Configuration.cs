c0e71c56532a9cfc8de0bb29eee8e6bd86a34a62
Chien Zhang <d3ara1n@outlook.com>
2026-05-11 16:27:50 +0800

    refactor: auto update check is disabled on linux

--- src/Polymerium.App/Configuration.cs
+++ src/Polymerium.App/Configuration.cs
@@ -69,28 +69,21 @@ public class Configuration
         { NETWORK_PROXY_PORT, 7890u },
         { NETWORK_PROXY_USERNAME, string.Empty },
         { NETWORK_PROXY_PASSWORD, string.Empty },
-        { UPDATE_AUTO_CHECK, true },
+        { UPDATE_AUTO_CHECK, !OperatingSystem.IsLinux() },
         { UPDATE_SOURCE, 1 },
         { UPDATE_MIRRORCHYAN_CDK, string.Empty },
     };
 
     public static string[] SupportedLanguages { get; } = ["en-US", "zh-Hans"];
 
-    public bool ApplicationSuperPowerActivated { get; set; } =
-        AccessDefault<bool>(APPLICATION_SUPERPOWER_ACTIVATED);
+    public bool ApplicationSuperPowerActivated { get; set; } = AccessDefault<bool>(APPLICATION_SUPERPOWER_ACTIVATED);
 
-    public bool ApplicationTitleBarVisibility { get; set; } =
-        AccessDefault<bool>(APPLICATION_TITLEBAR_VISIBILITY);
-    public bool ApplicationLeftPanelMode { get; set; } =
-        AccessDefault<bool>(APPLICATION_LEFT_PANEL_MODE);
-    public AccentColor ApplicationStyleAccent { get; set; } =
-        AccessDefault<AccentColor>(APPLICATION_STYLE_ACCENT);
-    public CornerStyle ApplicationStyleCorner { get; set; } =
-        AccessDefault<CornerStyle>(APPLICATION_STYLE_CORNER);
-    public int ApplicationStyleBackground { get; set; } =
-        AccessDefault<int>(APPLICATION_STYLE_BACKGROUND);
-    public int ApplicationStyleThemeVariant { get; set; } =
-        AccessDefault<int>(APPLICATION_STYLE_THEME_VARIANT);
+    public bool ApplicationTitleBarVisibility { get; set; } = AccessDefault<bool>(APPLICATION_TITLEBAR_VISIBILITY);
+    public bool ApplicationLeftPanelMode { get; set; } = AccessDefault<bool>(APPLICATION_LEFT_PANEL_MODE);
+    public AccentColor ApplicationStyleAccent { get; set; } = AccessDefault<AccentColor>(APPLICATION_STYLE_ACCENT);
+    public CornerStyle ApplicationStyleCorner { get; set; } = AccessDefault<CornerStyle>(APPLICATION_STYLE_CORNER);
+    public int ApplicationStyleBackground { get; set; } = AccessDefault<int>(APPLICATION_STYLE_BACKGROUND);
+    public int ApplicationStyleThemeVariant { get; set; } = AccessDefault<int>(APPLICATION_STYLE_THEME_VARIANT);
     public string ApplicationLanguage { get; set; } = AccessDefault<string>(APPLICATION_LANGUAGE);
     public string RuntimeJavaHome8 { get; set; } = AccessDefault<string>(RUNTIME_JAVA_HOME_8);
     public string RuntimeJavaHome11 { get; set; } = AccessDefault<string>(RUNTIME_JAVA_HOME_11);
@@ -98,8 +91,7 @@ public class Configuration
     public string RuntimeJavaHome21 { get; set; } = AccessDefault<string>(RUNTIME_JAVA_HOME_21);
     public string RuntimeJavaHome25 { get; set; } = AccessDefault<string>(RUNTIME_JAVA_HOME_25);
     public uint GameJavaMaxMemory { get; set; } = AccessDefault<uint>(GAME_JAVA_MAX_MEMORY);
-    public string GameJavaAdditionalArguments { get; set; } =
-        AccessDefault<string>(GAME_JAVA_ADDITIONAL_ARGUMENTS);
+    public string GameJavaAdditionalArguments { get; set; } = AccessDefault<string>(GAME_JAVA_ADDITIONAL_ARGUMENTS);
     public string GameCommandWrapper { get; set; } = AccessDefault<string>(GAME_COMMAND_WRAPPER);
     public uint GameWindowInitialWidth { get; set; } = AccessDefault<uint>(GAME_WINDOW_WIDTH);
     public uint GameWindowInitialHeight { get; set; } = AccessDefault<uint>(GAME_WINDOW_HEIGHT);
@@ -108,14 +100,11 @@ public class Configuration
     public bool NetworkProxyEnabled { get; set; } = AccessDefault<bool>(NETWORK_PROXY_ENABLED);
     public string NetworkProxyAddress { get; set; } = AccessDefault<string>(NETWORK_PROXY_ADDRESS);
     public uint NetworkProxyPort { get; set; } = AccessDefault<uint>(NETWORK_PROXY_PORT);
-    public string NetworkProxyUsername { get; set; } =
-        AccessDefault<string>(NETWORK_PROXY_USERNAME);
-    public string NetworkProxyPassword { get; set; } =
-        AccessDefault<string>(NETWORK_PROXY_PASSWORD);
+    public string NetworkProxyUsername { get; set; } = AccessDefault<string>(NETWORK_PROXY_USERNAME);
+    public string NetworkProxyPassword { get; set; } = AccessDefault<string>(NETWORK_PROXY_PASSWORD);
     public bool UpdateAutoCheck { get; set; } = AccessDefault<bool>(UPDATE_AUTO_CHECK);
     public int UpdateSource { get; set; } = AccessDefault<int>(UPDATE_SOURCE);
-    public string UpdateMirrorChyanCdk { get; set; } =
-        AccessDefault<string>(UPDATE_MIRRORCHYAN_CDK);
+    public string UpdateMirrorChyanCdk { get; set; } = AccessDefault<string>(UPDATE_MIRRORCHYAN_CDK);
 
     public static T AccessDefault<T>(string key) => (T)Defaults[key]!;
 }
