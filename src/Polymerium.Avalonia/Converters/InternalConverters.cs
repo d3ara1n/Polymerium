7b87acdba0f9d4463e08447a26283a69146aa446
Chien Zhang <d3ara1n@outlook.com>
2026-09-02 21:09:17 +0800

    fix(Avalonia): migrate disabled styles from removed opacity resource

--- src/Polymerium.Avalonia/Converters/InternalConverters.cs
+++ src/Polymerium.Avalonia/Converters/InternalConverters.cs
@@ -12,16 +12,6 @@ namespace Polymerium.Avalonia.Converters;
 
 public static class InternalConverters
 {
-    public static IMultiValueConverter OneOr { get; } = new RelayMultiConverter((v, _, _) =>
-    {
-        if (v is [bool b, double d])
-        {
-            return b ? 1.0d : d;
-        }
-
-        return 1.0d;
-    });
-
     public static IMultiValueConverter ZeroOr { get; } = new RelayMultiConverter((v, _, _) =>
     {
         if (v is [bool b, double d])
